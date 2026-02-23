acdf813cee8be398eda94153225082b2b58d3444
raum51 <raum51at@gmail.com>
2026-02-23 09:54:54 +0100

    Download-Toggle-Icon

--- src/DependencyInjection/Raum51ContaoZoteroExtension.php
+++ src/DependencyInjection/Raum51ContaoZoteroExtension.php
@@ -7,6 +7,7 @@ namespace Raum51\ContaoZoteroBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
@@ -16,9 +17,25 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
  *
  * Messenger-Routing: ZoteroSyncMessage implementiert LowPriorityMessageInterface –
  * Contao routet automatisch auf contao_prio_low. Keine Projekt-Config nötig.
+ *
+ * Asset-Package: raum51contaozotero wird für DCA-Operation-Icons u. a. benötigt
+ * (z. B. getUrl('icons/cloud-download.svg', 'raum51contaozotero')).
  */
-final class Raum51ContaoZoteroExtension extends Extension
+final class Raum51ContaoZoteroExtension extends Extension implements PrependExtensionInterface
 {
+    public function prepend(ContainerBuilder $container): void
+    {
+        $container->prependExtensionConfig('framework', [
+            'assets' => [
+                'packages' => [
+                    'raum51contaozotero' => [
+                        'base_path' => '/bundles/raum51contaozotero',
+                    ],
+                ],
+            ],
+        ]);
+    }
+
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader(
